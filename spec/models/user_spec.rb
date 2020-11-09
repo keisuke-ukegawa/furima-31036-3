c799efe8ba7395235629013662ecb78a2bbc2d8b
keisuke-ukegawa <dero_prot@icloud.com>
2020-11-09 14:28:12 +0900

    テストコード記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe '新規登録' do
+    it "nicknameが空だと登録できない" do
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "emailが空では登録できない" do
+      @user.email = ""
+      @user.valid?
+      binding.pry
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+  end
 end
